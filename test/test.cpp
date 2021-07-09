74a716b01cf092b3ec5551f76f84b240999dfdbc
Keshav Parwal <kparwal260@gmail.com>
2021-07-08 21:16:21 -0400

    Tabs to spaces
    spaces > tabs, fight me

--- test/test.cpp
+++ test/test.cpp
@@ -14,40 +14,40 @@
 class StatFixture : public ::testing::Test
 {
     protected:
-	int rc = -99999;
-	struct stat sb = {0};
-	const char *real_file = "file.txt";
-	const char *fake_file = "nonexistent.txt";
+    int rc = -99999;
+    struct stat sb = {0};
+    const char *real_file = "file.txt";
+    const char *fake_file = "nonexistent.txt";
 
-	bool caught_error = false;
-	void catch_error(int rc, const char *msg) {
-		caught_error = true;	
-	}
+    bool caught_error = false;
+    void catch_error(int rc, const char *msg) {
+        caught_error = true;
+    }
 
-	void SetUp() {
-		FILE *fp;
-		fp = fopen(real_file, "w");
-		fclose(fp);
-	}
+    void SetUp() {
+        FILE *fp;
+        fp = fopen(real_file, "w");
+        fclose(fp);
+    }
 
-	void TearDown() {
-		std::remove(real_file);
-	}
+    void TearDown() {
+        std::remove(real_file);
+    }
 };
 
 TEST_F(StatFixture, TestCheckSyscallSuccess) {
-	CHECK_SYSCALL(stat, rc, 0, catch_error, real_file, &sb);
-	ASSERT_TRUE(rc == 0);
-	ASSERT_FALSE(caught_error);
+    CHECK_SYSCALL(stat, rc, 0, catch_error, real_file, &sb);
+    ASSERT_TRUE(rc == 0);
+    ASSERT_FALSE(caught_error);
 }
 
 TEST_F(StatFixture, TestCheckSyscallFailure) {
-	ASSERT_FALSE(caught_error);
-	CHECK_SYSCALL(stat, rc, 0, catch_error, fake_file, &sb);
-	// stat sets errno instead of properly returning an rcode
-	ASSERT_TRUE(errno == 2);
-	ASSERT_TRUE(rc == -1);
-	ASSERT_TRUE(caught_error);
+    ASSERT_FALSE(caught_error);
+    CHECK_SYSCALL(stat, rc, 0, catch_error, fake_file, &sb);
+    // stat sets errno instead of properly returning an rcode
+    ASSERT_TRUE(errno == 2);
+    ASSERT_TRUE(rc == -1);
+    ASSERT_TRUE(caught_error);
 }
 
 int main(int argc, char **argv) {
